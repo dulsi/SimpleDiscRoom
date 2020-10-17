10c2e9dff055099e107bc37691969048fbe3098c
Dennis Payne <dulsi@identicalsoftware.com>
2020-10-17 09:09:50 -0400

    Do not slow discs down.

--- Disc.gd
+++ Disc.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
 
 
-export var min_speed = 150  # Minimum speed range.
-export var max_speed = 250  # Maximum speed range.
+export var min_speed = 250  # Minimum speed range.
+export var max_speed = 350  # Maximum speed range.
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +14,11 @@ func _process(delta):
 	$AnimatedSprite.rotation -= 3.14 * delta
 	if position.x < 0:
 		linear_velocity.x = abs(linear_velocity.x)
-	elif position.x > screen_size.x:
+	elif position.x > screen_size.x && linear_velocity.x > 0:
 		linear_velocity.x = -1 * linear_velocity.x
 	if position.y < 0:
 		linear_velocity.y = abs(linear_velocity.y)
-	elif position.y > screen_size.y:
+	elif position.y > screen_size.y && linear_velocity.y > 0:
 		linear_velocity.y = -1 * linear_velocity.y
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, 0, screen_size.x - 1)
+	position.y = clamp(position.y, 0, screen_size.y - 1)
